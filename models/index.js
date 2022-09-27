0de05d5a4d97ac12c690da0f8168f280f98db7d2
Belkbh <belkbh@gmail.com>
2022-09-27 14:57:37 -0400

    Started Model.js's

--- models/index.js
+++ models/index.js
@@ -1,5 +1,33 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
 // TODO: ADD RELATIONSHIPS
 
-module.exports = { User };
+Post.belongsTo(User, {
+  foreignKey: "user_id"
+});
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
