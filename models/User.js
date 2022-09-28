e8decae8f2fc71a35f194cb50ed3c2ce3bc50eed
Belkbh <belkbh@gmail.com>
2022-09-27 21:25:21 -0400

    Added seeds

--- models/User.js
+++ models/User.js
@@ -16,7 +16,7 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -32,8 +32,12 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8],
+        len: [6],
       },
+    },
+    profile_image_filename: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     
   },
