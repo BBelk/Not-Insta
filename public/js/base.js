c09cfc20d70e867210e1a4d4236945c37f727c98
Belkbh <belkbh@gmail.com>
2022-09-30 09:28:37 -0400

    updates in office hours

--- public/js/base.js
+++ public/js/base.js
@@ -1,16 +1,12 @@
 const imageUploadEl = document.querySelector('#image');
 
 imageUploadEl.addEventListener('change', async (event) => {
-  try{
   const file = event.target.files[0];
   const formData = new FormData();
-  const config = { 
-    withCredentials: true
-  };       
-  formData.append("file", file);
+  const config = { withCredentials: true };
+  formData.append('file', file);
   const item = await axios.post('/api/upload', formData, config);
-  if (item) location.reload();
-} catch(err){
-  console(err);
-}
+  if (item) {
+    location.reload();
+  }
 });
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-require("dotenv").config();
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
+require('dotenv').config();
+const path = require('path');
+const { existsSync, mkdirSync } = require('fs');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
-const routes = require("./controllers");
-const helpers = require("./utils/helpers");
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+let routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,17 +24,24 @@ const sess = {
   }),
 };
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(session(sess));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`);
+    // Creates a temporary directory if it doesn't exist
+    const dir = path.join(__dirname, 'tmp/');
+    if (!existsSync(dir)) {
+      mkdirSync(dir, 0744);
+    }
+  });
 });
