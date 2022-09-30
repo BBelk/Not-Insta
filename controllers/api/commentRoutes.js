dac9b5209db8922d43a7ebd27e1257a9a9191a25
Belkbh <belkbh@gmail.com>
2022-09-30 16:21:54 -0400

    Got comment user data to show up

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
-const { Comment } = require('../../models'); 
+const { Comment, User } = require('../../models'); 
 
+// {model: Comment,
+//   include: 
+//     {model: User,
+//     as: "commentUser",
+//   raw:false}
+//   }
+router.get('/get/id', async (req, res) => {
+  try{
+    const commentData = await Comment.findByPk(req.params.id, {
+      include: [
+        {model: User,
+        as: "commentUser"}
+      ]
+    });
+    const comment = commentData.get({ plain: true });
+    res.status(200).json(comment);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 router.put('/:id', async (req, res) => {
   try {
