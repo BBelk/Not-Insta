7c0fb6b03f6747320843a2cdc5a70627b45bca41
Nick <nsantander1095@gmail.com>
2022-10-04 10:48:34 -0400

    readme, followees on all pages, custom css, fixed errors, rest is formatting

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,26 +1,28 @@
 // const imageUploadEl = document.querySelector('#image');
 // const captionEl = document.querySelector('#body-post');
 const imageUploadEl = document.querySelector('#toUploadImage');
+if(imageUploadEl) {
+  imageUploadEl.addEventListener('change', async (event) => {
+    const userId = document.querySelector('#userIdStorage').innerHTML;
+    console.log("HELLO WORLD", userId);
+    // return;
+    const file = event.target.files[0];
+    if(file.size > 10000000){
+      alert("File too large, limit 10mb");
+      return;
+    }
+    const formData = new FormData();
+    const config = { withCredentials: true };
+    formData.append('file', file);
+    const item = await axios.post(`/api/upload/p/${userId}`, formData, config);
+    if (item) {
+      // let newId = item.data.id;
+      // console.log("HERES THE ITEM", item);
+      window.location.replace(`/profile`);
+    }
+  });
+}
 
-imageUploadEl.addEventListener('change', async (event) => {
-  const userId = document.querySelector('#userIdStorage').innerHTML;
-  console.log("HELLO WORLD", userId);
-  // return;
-  const file = event.target.files[0];
-  if(file.size > 10000000){
-    alert("File too large, limit 10mb");
-    return;
-  }
-  const formData = new FormData();
-  const config = { withCredentials: true };
-  formData.append('file', file);
-  const item = await axios.post(`/api/upload/p/${userId}`, formData, config);
-  if (item) {
-    // let newId = item.data.id;
-    // console.log("HERES THE ITEM", item);
-    window.location.replace(`/profile`);
-  }
-});
 
 // const ProfilePicUpload = async (event) =>{
 //   const userId = document.querySelector('#userIdStorage').innerHTML;
